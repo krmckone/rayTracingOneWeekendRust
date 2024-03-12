8756774fb8f68f9fdc87c1c52379af7403396070
Kaleb McKone <20476319+krmckone@users.noreply.github.com>
2024-03-12 16:57:22 -0400

    Zoom in

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use camera::Camera;
 use color::make_color;
 use material::{make_dielectric, make_lambertian, make_metal};
 use sphere::make_sphere;
+use vec3::Vec3;
 
 use crate::{hittable_list::HittableList, vec3::make_point};
 
@@ -25,18 +26,35 @@ fn main() {
         objects: Vec::new(),
     };
 
-    let r = (PI / 4.0).cos();
-    let material_left = make_lambertian(make_color(0.0, 0.0, 1.0));
-    let material_right = make_lambertian(make_color(1.0, 0.0, 0.0));
+    let material_ground = make_lambertian(make_color(0.8, 0.8, 0.0));
+    let material_center = make_lambertian(make_color(0.1, 0.2, 0.5));
+    let material_left = make_dielectric(1.5);
+    let material_left_bubble = make_dielectric(1.5);
+    let material_right = make_metal(make_color(0.8, 0.6, 0.2), 0.0);
 
     world.add(Box::new(make_sphere(
-        make_point(-r, -0.0, -1.0),
-        r,
+        make_point(0.0, -100.5, -1.0),
+        100.0,
+        Box::new(material_ground),
+    )));
+    world.add(Box::new(make_sphere(
+        make_point(0.0, 0.0, -1.0),
+        0.5,
+        Box::new(material_center),
+    )));
+    world.add(Box::new(make_sphere(
+        make_point(-1.0, 0.0, -1.0),
+        0.5,
         Box::new(material_left),
     )));
     world.add(Box::new(make_sphere(
-        make_point(r, 0.0, -1.0),
-        r,
+        make_point(-1.0, 0.0, -1.0),
+        -0.4,
+        Box::new(material_left_bubble),
+    )));
+    world.add(Box::new(make_sphere(
+        make_point(1.0, 0.0, -1.0),
+        0.5,
         Box::new(material_right),
     )));
 
@@ -46,7 +64,11 @@ fn main() {
     camera.image_width = 400;
     camera.samples_per_pixel = 100;
     camera.max_depth = 50;
-    camera.vfov = 90;
+
+    camera.vfov = 20;
+    camera.lookfrom = make_point(-2.0, 2.0, 1.0);
+    camera.lookat = make_point(0.0, 0.0, -1.0);
+    camera.vup = Vec3(0.0, 1.0, 0.0);
 
     camera.render(&world);
 }
